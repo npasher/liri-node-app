241b3e452e1ad505ca1d599aa7b391a1ee5d8e2c
npasher <n.p.asher@gmail.com>
2018-04-24 14:36:10 -0400

    more code

--- liri.js
+++ liri.js
@@ -1,10 +1,22 @@
 
 require("dotenv").config();
-import { twitter } from "./keys";
+// import { twitter } from "./keys";
 
-var fs=require("fs");
+let fs=require("fs");
+let request=require("request");
+let keys=require("./keys.js");
+let twitter=require("twitter");
+let spotify=require("spotify-web-api-node");
+let userCommand=process.argv[2];
+let userInput=process.argv[3];
 
-function tweets(){
-  var client=new twitter(keys.twitterKeys);
-  var 
-}
+function myTweets(){
+  let twitterClient=new twitter(keys.twitterKeys);
+  let twitterUser=userInput;
+  let words="text";
+  let params={screen_name:twitterUser, count: 20};
+  twitterClient.get("statuses/user_timeline",params,function(error,tweets,response){
+    console.log("Here are the last 20 Tweets for:" +twitterUser);
+    if(error) throw error;
+  });
+};
